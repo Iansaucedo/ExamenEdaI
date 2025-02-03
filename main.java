a5cc2049b911ecc38e87d8b1453d40814d01cc2b
ian.saucedo <ian.saucedo@ct.uneatlantico.es>
2025-02-03 16:23:05 +0100

    drawing a circle

--- main.java
+++ main.java
@@ -2,39 +2,38 @@
 
 public class main {
   public static void main(String[] args) throws InterruptedException {
-    
     RendererEngine engine = new RendererEngine(new BufferFrame());
     engine.setMode("color");
 
     int totalRows = 10;
     int totalCols = 40;
-    int frames = 60;
+    int frames = 110;
 
     FrameManager frameManager = new FrameManager();
-    frameManager.refreshRate = 1000;
-
+    frameManager.refreshRate = 10;
 
     int centerY = totalRows / 2;
     int centerX = totalCols / 2;
-    int radius = Math.min(totalRows, totalCols) / 2;
-
+    int minRadius = 2;
+    int maxRadius = Math.min(totalRows, totalCols) / 2;
 
     char[] colors = {Pixel.NEGRO, Pixel.BLANCO, Pixel.CYAN, Pixel.MAGENTA};
 
     for (int frame = 0; frame < frames; frame++) {
-
       engine = new RendererEngine(new BufferFrame());
       engine.setMode("color");
 
-   
       char currentColor = colors[frame % colors.length];
+
+  
+      double oscillation = Math.sin((double) frame / frames * 2 * Math.PI);
+      int radius = (int) ((maxRadius - minRadius) / 2 * (oscillation + 1) + minRadius);
 
       for (int y = 0; y < totalRows; y++) {
         for (int x = 0; x < totalCols; x++) {
           Instruction instr = new Instruction();
           Pixel p = new Pixel();
 
-       
           int distanceSquared = (y - centerY) * (y - centerY) + (x - centerX) * (x - centerX);
           char pixelChar = (distanceSquared <= radius * radius) ? currentColor : ' ';
 
@@ -48,12 +47,13 @@ public class main {
       }
 
       engine.DrawFrame();
-
+      
       Frame bufferedFrame = engine.SendFrame();
       if (bufferedFrame != null) {
         frameManager.Send(bufferedFrame);
         clearScreen();
         new Renderer().Show(bufferedFrame);
+       
       }
 
       Thread.sleep(frameManager.refreshRate);
